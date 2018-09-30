ff9a15b1bc1ae9cd0c9a7b0afbf51cd6ec7498ab
José Nunes <josenunes34@gmail.com>
2018-09-30 17:17:25 +0100

    Remove hyper-power plugin

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperminimal', 'hyperpower', 'hyper-transparent'],
+  plugins: ['hyperminimal', 'hyper-transparent'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
