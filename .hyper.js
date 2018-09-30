d9353badccfdbcf74de3be5f2e0e75031aa52e19
José Nunes <josenunes34@gmail.com>
2018-09-30 17:36:28 +0100

    Remove hyper-transparent (causes background bug)

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperminimal', 'hyper-transparent'],
+  plugins: ['hyperminimal'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
